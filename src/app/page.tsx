cb029d8e4b16b4e9e975e403e2f812096787799c
aditya-hansraj <04adityahansraj@gmail.com>
2024-08-02 11:31:27 +0530

    debug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import HeroSection from "@/components/HeroSection";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.2]">
-      <h1 className="text-6xl text-center">Hello World</h1>
+      <HeroSection />
     </main>
   );
 }
