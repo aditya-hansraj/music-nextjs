7804230cf35ff3fd2ce0e0967c219a237961b5d7
aditya-hansraj <04adityahansraj@gmail.com>
2024-08-06 08:47:45 +0530

    Added Instructors Section and Wavy BG

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
 import TestimonialCards from "@/components/TestimonialCards";
 import Webinars from "@/components/Webinars";
 import WhyChooseUs from "@/components/WhyChooseUs";
@@ -12,6 +13,7 @@ export default function Home() {
       <WhyChooseUs />
       <TestimonialCards />
       <Webinars />
+      <Instructors />
     </main>
   );
 }
